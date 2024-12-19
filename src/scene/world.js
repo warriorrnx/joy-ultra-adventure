173b0199039fa6f6de2a1166c85f059f801e3ff2
warriorrnx <softdumb2009@gmail.com>
2024-12-19 15:53:43 +0000

    m

--- src/scene/world.js
+++ src/scene/world.js
@@ -1,5 +1,28 @@
 import { fetchMapData } from "../utils.js";
 
 export default async function world(k) {
+  colorizeBackground(k, 76, 170, 255);
   const mapData = await fetchMapData("./assets/maps/world.json");
+
+  const map = k.add([k.pos(0, 0)]);
+
+  const entities = {
+    player: null,
+    slimes: [],
+  };
+
+  const layers = mapData.layers;
+  for (const layer of layers) {
+    if (layer.name === "Boundaries") {
+      //TODO
+      continue;
+    }
+
+    if (layer.name === "Spawnpoints") {
+      //TODO
+      continue;
+    }
+
+    //desenha todos os tiles
+  }
 }
