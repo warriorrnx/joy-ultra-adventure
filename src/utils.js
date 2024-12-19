bc9d1fcc843e3c608f1217d5f3573977c49bdf1f
warriorrnx <softdumb2009@gmail.com>
2024-12-19 16:16:03 +0000

    v

--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,21 @@ export function colorizeBackground(k, r, g, b) {
 export async function fetchMapData(mapPath) {
   return await (await fetch(mapPath)).json();
 }
+
+export function drawTiles(k, map, layer, tileheight, tilewidth) {
+    let nbOfDrawTiles = 0;
+    const tilePos = k.vec2(0, 0);
+    for (const tile of layer.data) {
+        if (nbOfDrawTiles % layer.width === 0) {
+            tilePos.x = 0;
+            tilePos.y += tileHeight;
+        } 
+
+        nbOfDrawTiles++;
+        if (tile === 0) continue;
+
+        map.add([
+            k.sprite("assets", { frame: tile - 1})
+        ])
+    }
+}
