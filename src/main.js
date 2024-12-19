68e95a2d22bb713325393d5f1ec449439f848599
warriorrnx <softdumb2009@gmail.com>
2024-12-19 13:46:50 +0000

    v

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 //isso importa a const do kaboom do arquivo kabbomContext.js
 import k from "./kaboomContext.js";
+import world from "./scene/world.js";
 
+//variavel que armazena as cenas ou scenes
+const scenes = {
+  world,
+};
+
+for (const sceneName in scenes) {
+  k.scene(sceneName, () => scenes[sceneName](k));
+}
+
+k.go("world");
