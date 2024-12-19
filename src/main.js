97fb869e96fc43fb08e6fa2897d5991c3ff94284
warriorrnx <softdumb2009@gmail.com>
2024-12-19 13:16:02 +0000

    v

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 //isso importa a const do kaboom do arquivo kabbomContext.js 
-import k from "./kaboomContext.js"
+import k from "./kaboomContext.js";
 import world from ".scene/world.js";
 
 
 const scenes = {
     world,
-}
+};
 
 for (const sceneName in scenes) {
     k.scene(sceneName, () => scenes[sceneName](k));        
 }
 
-k.go("world")
+k.go("world");
--- src/scene/world.js
+++ src/scene/world.js
@@ -1,3 +1,3 @@
-export default async function world(k){
-    
+export default async function world(k) {
+
 }
